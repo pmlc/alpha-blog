8d4113cf2760ca1b0529ceb075b94064a4477005
Pierre Le Count <pmlc@iinet.net.au>
2018-07-10 06:05:44 +0000

    Color picker added to user page

---
+++ app/javascript/components/Colorpicker.js
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import { SketchPicker } from 'react-color'; 
+	    
+class Colorpicker extends React.Component {
+      render() {
+        return (
+        <React.Fragment>
+            <SketchPicker />
+        </React.Fragment>
+        );
+    }
+ }
+	 
+Colorpicker.propTypes = {};
+	 
+export default Colorpicker
